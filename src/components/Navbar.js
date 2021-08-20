d5d13cd402aecc321a074389768a9864e1a64b74
Dmitriy <dmitriy.babich0@gmail.com>
2021-08-19 20:56:27 -0700

    nav bar styles

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,21 +1,22 @@
 import React from 'react';
 import { BrowserRouter as Router, Link} from "react-router-dom";
+import '../App.css'
 
 
 const Navbar = () => {
     return (
         <Router>
             <div className="navbar-container">
-                <h2 className="nav-logo">Portfolio</h2>
+                <h2 className="nav-logo"> <span className='blue-text'>Dmitriy</span> Babich</h2>
                 <ul className='nav-ul'>
                     <li className="nav-li">
-                      <Link to='about'>About</Link>
+                      <Link to='about' className='nav-link'>About me</Link>
                     </li>
                     <li className="nav-li">
-                      <Link to='work'>My Work</Link>
+                      <Link to='work' className='nav-link'>My work</Link>
                     </li>
                     <li className="nav-li">
-                      <Link to='contact'>Contact Me</Link>
+                      <Link to='contact' className='nav-link'>Contact me</Link>
                     </li>
                 </ul>
             </div>
