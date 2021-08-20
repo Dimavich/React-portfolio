b66af155c1572f1e188df1b565179ac5ec481385
Dmitriy <dmitriy.babich0@gmail.com>
2021-08-19 21:49:43 -0700

    hero page component and styles

---
+++ src/components/Hero.js
@@ -0,0 +1,17 @@
+import React from 'react'
+
+function Hero() {
+    return (
+        <div className="hero-container">
+            <div className="hero-title">
+                <h3 className='hero-small-text hero-text'>Hello Im a</h3>
+                <h2 className="hero-large-text">Web Developer</h2>
+            </div>
+            <div className="about-me-container">
+                <p className="about-me-text">I am an aspiring full stack developer. I started my software developer journey about 9 months ago and learn more and more each day. I have experience in graphic design and my mission is to incorporate functionality with good design.</p>
+            </div>
+        </div>
+    )
+}
+
+export default Hero
