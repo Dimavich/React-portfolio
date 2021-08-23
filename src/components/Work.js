070bbe4c535c1a713ea13e144ffa404adea61a3f
Dmitriy <dmitriy.babich0@gmail.com>
2021-08-22 22:26:40 -0700

    added work title and several smaller tweaks to the carousel

--- src/components/Work.js
+++ src/components/Work.js
@@ -36,71 +36,70 @@ class MyCarousel extends Component {
   }
 
   render() {
-    return (
-        <div className="work-container">
-            <div style={MyCarousel.CONTAINER_STYLE}>
-                <ReactCardCarousel autoplay={false} autoplay_speed={5000}>
-                <div style={MyCarousel.CARD_STYLE}>
-                    <div className="card-text">
-                        <h3 className="card-title">Eventory</h3>
-                        <Router>
-                            <div className="link-icons">
-                                <Link to='#' className='visit-link icon-link'>
-                                    <i className="far fa-window-maximize icon"></i>
-                                    <p className="icontext">Visit</p>
-                                </Link>
-                                <Link to='#' className='visit-link icon-link'>
-                                    <i class="fab fa-github"></i>
-                                    <p className="icontext">GitHub</p>
-                                </Link>
-                            </div>
-                        </Router>
+    return ( 
+        <div className='title-container'>
+            <h2 className="page-title"> Some of my <span className="red-text">Work</span></h2>
+            <div className="work-container">
+                <div style={MyCarousel.CONTAINER_STYLE}>
+                    <ReactCardCarousel autoplay={true} autoplay_speed={5000}>
+                    <div style={MyCarousel.CARD_STYLE}>
+                        <div className="card-text">
+                            <h3 className="card-title">Eventory</h3>
+                            <Router>
+                                <div className="link-icons">
+                                    <Link href='https://dimavich.github.io/web-portfolio/#2' className='visit-link icon-link'>
+                                        <i className="far fa-window-maximize icon"></i>
+                                        <p className="icontext">Visit</p>
+                                    </Link>
+                                    <Link to='https://github.com/Dimavich/Eventory' className='visit-link icon-link'>
+                                        <i class="fab fa-github"></i>
+                                        <p className="icontext">GitHub</p>
+                                    </Link>
+                                </div>
+                            </Router>
+                        </div>
+                        <div className="card-image">
+                            <img src={eventory} alt='screenshot of my work' />
+                        </div>
                     </div>
-                    <div className="card-image">
-                        <img src={eventory} alt='screenshot of my work' />
+                    <div style={MyCarousel.CARD_STYLE}>
+                        <div className="card-text">
+                            <h3 className="card-title">Surf Forecast</h3>
+                            <Router>
+                                <div className="link-icons">
+                                    <Link to='https://dimavich.github.io/surf-forecast/' className='visit-link icon-link'>
+                                        <i className="far fa-window-maximize icon"></i>
+                                        <p className="icontext">Visit</p>
+                                    </Link>
+                                    <Link to='https://github.com/Dimavich/surf-forecast' className='visit-link icon-link'>
+                                        <i class="fab fa-github"></i>
+                                        <p className="icontext">GitHub</p>
+                                    </Link>
+                                </div>
+                            </Router>
+                        </div>
+                        <div className="card-image">
+                            <img src={surf} alt='screenshot of my work' />
+                        </div>
                     </div>
+                    <div style={MyCarousel.CARD_STYLE}>
+                        <div className="card-text">
+                            <h3 className="card-title">Team Dash</h3>
+                            <Router>
+                                <div className="link-icons">
+                                    <Link to='https://github.com/Dimavich/Team-card-generator' className='visit-link icon-link'>
+                                        <i class="fab fa-github"></i>
+                                        <p className="icontext">GitHub</p>
+                                    </Link>
+                                </div>
+                            </Router>
+                        </div>
+                        <div className="card-image">
+                            <img src={teamDash} alt='screenshot of my work' />
+                        </div>
+                    </div>
+                    </ReactCardCarousel>
                 </div>
-                <div style={MyCarousel.CARD_STYLE}>
-                    <div className="card-text">
-                        <h3 className="card-title">Surf Forecast</h3>
-                        <Router>
-                            <div className="link-icons">
-                                <Link to='#' className='visit-link icon-link'>
-                                    <i className="far fa-window-maximize icon"></i>
-                                    <p className="icontext">Visit</p>
-                                </Link>
-                                <Link to='#' className='visit-link icon-link'>
-                                    <i class="fab fa-github"></i>
-                                    <p className="icontext">GitHub</p>
-                                </Link>
-                            </div>
-                        </Router>
-                    </div>
-                    <div className="card-image">
-                        <img src={surf} alt='screenshot of my work' />
-                    </div>
-                </div>
-                <div style={MyCarousel.CARD_STYLE}>
-                    <div className="card-text">
-                        <h3 className="card-title">Team Dash</h3>
-                        <Router>
-                            <div className="link-icons">
-                                <Link to='#' className='visit-link icon-link'>
-                                    <i className="far fa-window-maximize icon"></i>
-                                    <p className="icontext">Visit</p>
-                                </Link>
-                                <Link to='#' className='visit-link icon-link'>
-                                    <i class="fab fa-github"></i>
-                                    <p className="icontext">GitHub</p>
-                                </Link>
-                            </div>
-                        </Router>
-                    </div>
-                    <div className="card-image">
-                        <img src={teamDash} alt='screenshot of my work' />
-                    </div>
-                </div>
-                </ReactCardCarousel>
             </div>
         </div>
     );
